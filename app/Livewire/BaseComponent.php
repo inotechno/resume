4a12077a5835fe368784b09360cefd006b14c1f6
inotechno <2inotechno0@gmail.com>
2025-07-01 23:06:56 +0700

    [update] Add Favicon

--- app/Livewire/BaseComponent.php
+++ app/Livewire/BaseComponent.php
@@ -7,11 +7,9 @@ use Livewire\Component;
 
 class BaseComponent extends Component
 {
-    public $favicon;
     public $personal;
     public function boot()
     {
         $this->personal = PersonalInfo::all()->pluck('value', 'key')->toArray();
-        $this->favicon = $this->personal['favicon'];
     }
 }
--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -7,17 +7,19 @@ use Livewire\Component;
 class Home extends BaseComponent
 {
     public $darkMode = true;
+    public $favicon;
     public $colorNumber = 2;
 
     public function mount()
     {
         $this->darkMode = $this->personal['dark_mode'];
         $this->colorNumber = $this->personal['color_number'];
+        $this->favicon = $this->personal['favicon'];
     }
 
     public function render()
     {
         // dd($this->personal);
-        return view('livewire.home')->title('Resume | ' . $this->personal['name']);
+        return view('livewire.home')->title('Resume | ' . $this->personal['name'])->extends('layouts.app')->favicon($this->favicon);
     }
 }
